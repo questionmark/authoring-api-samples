8dbcae734a3ccc412a2da38fd9ce3aa6b38a763e
Manuel Salvatori <manuel.salvatori@questionmark.com>
2020-03-23 17:48:26 +0000

    Pushed HttpClient examples

---
+++ Examples/Extensions/Extensions.cs
@@ -0,0 +1,147 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.Extensions
+{
+    public class ODataMetadata
+    {
+        public Dictionary<string, string> Metadata = new Dictionary<string, string>();
+
+        public string GetNextLink()
+        {
+            Metadata.TryGetValue("nextLink", out string value);
+            var idx = value?.IndexOf("/odata/");
+            return value?.Substring(idx.Value + 7);
+        }
+
+        public string GetRawNextLink()
+        {
+            Metadata.TryGetValue("nextLink", out string value);
+            return value;
+        }
+
+        public string GetContent()
+        {
+            Metadata.TryGetValue("content", out string value);
+            return value;
+        }
+    }
+
+    public static class Extensions
+    {
+        /// <summary>
+        /// Get the list of ODataMetadata
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        public static ODataMetadata GetODataMetadata(this HttpResponseMessage response)
+        {
+            var result = new ODataMetadata();
+
+            var responseString = response.Content.ReadAsStringAsync().Result;
+
+            while (responseString.Contains("\"@odata"))
+            {
+                var idx = responseString.IndexOf("\"@odata");
+                responseString = responseString.Substring(idx + 8);
+
+                var name = GetODataMentadataName(responseString, out string newResponseString);
+
+                responseString = newResponseString;
+
+                var value = GetODataMentadataValue(responseString, out newResponseString);
+
+                responseString = newResponseString;
+
+                result.Metadata.Add(name, value);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get the message response content as the specified type T
+        /// </summary>
+        /// <typeparam name="T">Type used to deserialize the content of the response</typeparam>
+        /// <param name="response">HttpResponseMessage from where the Content is taken</param>
+        /// <returns>Content as the type specified</returns>
+        public static async Task<T> GetContentAsAsync<T>(this HttpResponseMessage response)
+        {
+            var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            // Convert collections
+            if (typeof(T).IsGenericType &&
+                (
+                    typeof(T).GetGenericTypeDefinition() == typeof(IEnumerable<>) ||
+                    typeof(T).GetGenericTypeDefinition() == typeof(ICollection<>) ||
+                    typeof(T).GetGenericTypeDefinition() == typeof(List<>)
+                ) &&
+                typeof(T).GenericTypeArguments.Any())
+            {
+                if (!responseString.Contains("\"@odata")) return JsonConvert.DeserializeObject<T>(responseString);
+
+                // Get value from the content
+                responseString = JObject.Parse(responseString)["value"].ToString();
+
+                var collectionType = typeof(ICollection<>);
+                var genericType = collectionType.MakeGenericType(typeof(T).GenericTypeArguments[0]);
+
+                return (T)JsonConvert.DeserializeObject(responseString, genericType);
+            }
+
+            // Convert any type not collection
+            return JsonConvert.DeserializeObject<T>(responseString);
+        }
+
+        public static async Task<string> GetContentRawContentAsStringAsync(this HttpResponseMessage response)
+        {
+            var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);                
+            return JObject.Parse(responseString)["value"].ToString();
+        }
+
+        public static string GetContentRawContentAsString(this HttpResponseMessage response)
+        {
+            return GetContentRawContentAsStringAsync(response).GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Sync method to get content deserialised as the specified generic type
+        /// </summary>
+        /// <typeparam name="T">Type of the expected result, as single or list of objects</typeparam>
+        /// <param name="response">HttpResponseMessage response from where the results has to be extracted</param>
+        /// <returns>Object or objects requested</returns>
+        public static T GetContentAs<T>(this HttpResponseMessage response)
+        {
+            return GetContentAsAsync<T>(response).GetAwaiter().GetResult();
+        }
+
+        private static string GetODataMentadataValue(string responseString, out string newResponseString)
+        {
+            var idx = responseString.IndexOf("\":\"");
+            responseString = responseString.Substring(idx + 3);
+
+            idx = responseString.IndexOf('"');
+
+            var result = responseString.Substring(0, idx);
+            newResponseString = responseString.Substring(idx + 1);
+
+            return result;
+        }
+
+        private static string GetODataMentadataName(string responseString, out string newResponseString)
+        {
+            var idx = responseString.IndexOf('"');
+            var result = responseString.Substring(0, idx);
+
+            newResponseString = responseString.Substring(idx + 1);
+
+            return result;
+        }
+    }
+}
---
+++ Examples/IAuthoringApiExamples.cs
@@ -0,0 +1,94 @@
+﻿using QM.AuthoringApi.OData.Entity;
+using System;
+using System.Collections.Generic;
+
+namespace Examples
+{
+    /// <summary>
+    /// Each method represent an example of request
+    /// </summary>
+    public interface IAuthoringApiExamples : IDisposable
+    {
+        /// <summary>
+        /// Get the assessment revision filtering by the AssessmentRevisionId
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>AssessmentRevision by AssessmentRevisionId</returns>
+        AssessmentRevision GetAssessmentRevisionById(int id);
+
+        /// <summary>
+        /// Get all AssessmentAML filtering by assessment revision Id
+        /// Note: The maximum amount of results is limited by the page size defined by the server
+        /// </summary>
+        /// <param name="id">AssessmentRevisionId</param>
+        /// <returns>All AssessmentAML with the specified AssessmentRevisionId</returns>
+        IEnumerable<AssessmentAML> GetAssessmentAMLsByAssessmentRevisionId(int id);
+
+        /// <summary>
+        /// Get all AML streams filtering by the Assessment Name
+        /// Note: The maximum amount of results is limited by the page size defined by the server
+        /// </summary>
+        /// <param name="assessmentName">Name of the assessment to search</param>
+        /// <returns>List of all AMLs</returns>
+        IEnumerable<string> GetAMLsByAssessmentName(string assessmentName);
+
+        /// <summary>
+        /// Get the AML with AssessmentRevisionId and Language
+        /// </summary>
+        /// <param name="id">AssessmentRevisionId</param>
+        /// <param name="language">Language</param>
+        /// <returns>AML of the corresponding Assessment</returns>
+        string GetAMLByAssessmentRevisionIdAndLanguage(int id, string language);
+
+        /// <summary>
+        /// Get QML by QuestionRevisionId and Language
+        /// </summary>
+        /// <param name="questionRevisionId">QuestionRevisionId</param>
+        /// <param name="language">Language</param>
+        /// <returns>QML</returns>
+        string GetQMLByQuestionRevisionIdAndLanguage(int questionRevisionId, string language);
+
+        /// <summary>
+        /// Get all QML streams filtering by the QuestionId (Id used in the delivery platform after publishing)
+        /// Note: The maximum amount of results is limited by the page size defined by the server
+        /// </summary>
+        /// <param name="questionId">Question identifier as known in the delivery system after published</param>
+        /// <returns>QML of each question with the given published Id</returns>
+        IEnumerable<string> GetQMLsByQuestionId(long questionId);
+
+        /// <summary>
+        /// Get all QML streams filtering by Topic Name
+        /// Note: The maximum amount of results is limited by the page size defined by the server
+        /// </summary>
+        /// <param name="topicName">Name of the topic where all questions belong to</param>
+        /// <returns>QML of each question belonging to the topic with the given name</returns>
+        IEnumerable<string> GetQMLsByTopicName(string topicName);
+
+        /// <summary>
+        /// Get all assessment names iterating through each page
+        /// </summary>
+        /// <returns>The name of each assessment</returns>
+        IEnumerable<string> GetAllAssessmentNamesIteratingEachAvailablePage();
+        
+        /// <summary>
+        /// Get list of Id of all sub topics of the topic with teh given Id
+        /// </summary>
+        /// <param name="topicId">Id of the topic</param>
+        /// <returns>Lit of the sub topics Ids</returns>
+        IEnumerable<int> GetSubTopicsIdOfTopic(int topicId);
+
+        /// <summary>
+        /// Get the latest QuestionRevision by Topic PublishedId
+        /// </summary>
+        /// <param name="publishedId">Published Id of the topic</param>
+        /// <returns>The last version of the question revision</returns>
+        IEnumerable<QuestionRevision> GetLatestQuestionRevisionsByTopicPublishedId(int publishedId);
+
+        /// <summary>
+        /// Get the latest QuestionRevision by QuestionId
+        /// </summary>
+        /// <param name="questionId">Question Id as appear in QML and AML</param>
+        /// <returns>The last version of the question revision</returns>
+        IEnumerable<QuestionRevision> GetLatestQuestionRevisionsByQuestionId(long questionId);
+    }
+}
